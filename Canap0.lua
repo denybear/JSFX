c85a6b12dffd0b2376a963ca2ef138e26523265e
denybear <98181634+denybear@users.noreply.github.com>
2024-07-11 22:28:59 +0200

    debug, improved robustness

--- Canap0.lua
+++ Canap0.lua
@@ -1,6 +1,15 @@
 -- wait function: wait for media item recording to be finalized; as soon it is finalized, go to endprocess()
 function wait_for_item()
 
+	-- make sure we are not in endless loop
+	if (max_loop < 50) then
+		max_loop = max_loop + 1
+	else
+		endprocess ()
+		return
+	end
+
+	sel_track = reaper.GetTrack(0, track_number)
 	itm = reaper.GetTrackMediaItem(sel_track, 0)
 	if not itm then
 		reaper.defer(wait_for_item)
@@ -14,9 +23,22 @@ end
 -- copy media item to the end of the track
 function endprocess ()
 
+	-- get the rec armed state of the first 8 tracks and restore it
+	reaper.ClearAllRecArmed() 							-- clear all armed tracks
+	for i = 0, 7, 1
+	do
+		param = "rec_armed" .. tostring (i)
+		rec_status = reaper.GetExtState(script_name, param)
+		if (rec_status == "armed") then
+			reaper.Main_OnCommand(25 + (i * 8), 0)	-- Arm track for recording
+		end
+	end
+
 	--get first and only media item in the track
 	item = reaper.GetTrackMediaItem(sel_track, 0)
-
+	if not item then
+		return								-- if no recorded data, leave
+	end
 
 	-- Compute beginning and end (in sec) of the recorded item
 	local first_sel_pos = reaper.GetMediaItemInfo_Value(item, 'D_POSITION')
@@ -24,7 +46,6 @@ function endprocess ()
 	local length = last_sel_end-first_sel_pos
 	-- str = "beg:" .. tostring (first_sel_pos) .. " end: " .. tostring (last_sel_end) .. "lg : " .. tostring (length)
 	-- reaper.ShowConsoleMsg(str)
-
 	
 	reaper.Main_OnCommand(40939 + track_number, 0)		-- Select track
 	reaper.Main_OnCommand(40421, 0)						-- Select all items in selected track
@@ -41,6 +62,7 @@ end
 
 -- MAIN
 -- Recording track 0
+max_loop = 0			-- allows to exit script if elapsed time is too long
 track_number = 0
 trk_number = tostring (track_number)
 script_name = "Deny"
@@ -49,6 +71,7 @@ rec_start_bar = "rec_start_bar" .. track_number
 rec_end_bar = "rec_end_bar" .. track_number
 
 -- test toggle state and set to starting value
+-- save state in memory to reuse it at a later stage (when the action is called again)
 if not reaper.HasExtState(script_name, rec_toggle_state) then
 	reaper.SetExtState(script_name, rec_toggle_state, "true", false)
 end
@@ -56,6 +79,20 @@ end
 -- test whether this is record ON or OFF
 if reaper.GetExtState(script_name, rec_toggle_state) == "true" then
 -- Record ON
+
+	-- save the rec armed state of the first 8 tracks; we will restore them after recording
+	for i = 0, 7, 1
+	do
+		-- write to memory
+		param = "rec_armed" .. tostring (i)
+		sel_track = reaper.GetTrack(0, i)
+		track_name, rec_status = reaper.GetTrackState(sel_track)
+		if ((rec_status & 0x40) == 0x40) then
+			reaper.SetExtState(script_name, param, "armed", false)
+		else
+			reaper.SetExtState(script_name, param, "dearmed", false)
+		end
+	end
 
 	-- delete any item in the track
 	-- (to do later: delete items that are after position)
@@ -103,7 +140,6 @@ else
 	reaper.SetExtState(script_name, rec_toggle_state, "true", false)
 
 	 -- check if recording is finish	
-	sel_track = reaper.GetTrack(0, track_number)
 	wait_for_item()
 	-- end of the process is done in endprocess() function
 	-- copy media item to the end of the track
